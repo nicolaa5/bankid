08f081f79bcbb6b739566ecbd45d990289320e46
nicolaa5 <niekbijman@gmail.com>
2024-04-24 16:02:46 +0200

    implement options pattern

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,12 @@
 package api
 
+// BankID is a go client for the BankID API
 type BankID interface {
-	// auth
+	// Initiates an authentication order. Use the collect method to query the status of the order. If the request is successful the response includes:
+	// - orderRef
+	// - autoStartToken
+	// - qrStartToken
+	// - qrStartSecret
 	Auth(personnummer string) error
 
 	// sign
@@ -14,8 +19,12 @@ type BankID interface {
 	PhoneSign() error
 
 	// collect
-	Collect() error
+	Collect(orderNummer string) error
 
 	// cancel
-	Cancel() error
+	Cancel(orderNummer string) error
+}
+
+func New(config Config) (*BankID, error) {
+	if config.URL
 }
