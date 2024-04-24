2031686bb3cf9f54ef02213302517c90c5689a4d
nicolaa5 <niekbijman@gmail.com>
2024-04-24 15:38:31 +0200

    add interface methods

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,9 +2,20 @@ package api
 
 type BankID interface {
 	// auth
+	Auth(personnummer string) error
+
 	// sign
+	Sign() error
+
 	// phone/auth
+	PhoneAuth() error
+
 	// phone/sign
+	PhoneSign() error
+
 	// collect
+	Collect() error
+
 	// cancel
+	Cancel() error
 }
