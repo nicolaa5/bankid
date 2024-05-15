77bd20509d808578a815f092bfcf8b30ae0d28cb
nicolaa5 <niekbijman@gmail.com>
2024-05-15 16:01:16 +0200

    encode and check input

--- options.go
+++ options.go
@@ -1,6 +1,10 @@
 package bankid
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+	"unicode/utf8"
+)
 
 
 type Option func(RequestBody) (RequestBody, error)
@@ -47,20 +51,48 @@ func WithUserVisibleData(userVisibleData string) Option {
     return func(rb RequestBody) (RequestBody, error) {
 		switch v := (rb).(type) {
 		case AuthRequest:
-			if userVisibleData == "" {
+			if userVisibleData == "" || !utf8.ValidString(userVisibleData) {
 				return nil, fmt.Errorf("Optional input is not set: UserVisibleData")
 			}
 
-			v.UserVisibleData = userVisibleData
+			encodedData := base64.StdEncoding.EncodeToString([]byte(userVisibleData))
+			v.UserVisibleData = encodedData
+			return v, nil
 
 		case SignRequest:
-			if userVisibleData == "" {
-				return nil, RequiredInputError{ Message: fmt.Sprintf("UserVisibleData is missing but required by BankID for request: %T", v)}
+			// required for sign requests
+			if userVisibleData == "" || !utf8.ValidString(userVisibleData) {
+				return nil, RequiredInputError{ Message: fmt.Sprintf("UserVisibleData is missing or invalid, it's required by BankID for request: %T", v)}
 			}
 
-			v.UserVisibleData = userVisibleData
+			encodedData := base64.StdEncoding.EncodeToString([]byte(userVisibleData))
+			v.UserVisibleData = encodedData
+			return v, nil
 		}
 		
 		return nil, fmt.Errorf("unkown type: %T", rb)
     }
+}
+
+func WithUserNonVisibleData(userNonVisibleData string) Option {
+	return func(rb RequestBody) (RequestBody, error) {
+
+		if userNonVisibleData == "" || !utf8.ValidString(userNonVisibleData) {
+			return nil, fmt.Errorf("Optional input is not set: UserNonVisibleData")
+		}
+
+		encodedData := base64.StdEncoding.EncodeToString([]byte(userNonVisibleData))
+
+		switch v := (rb).(type) {
+		case AuthRequest:
+			v.UserVisibleData = encodedData
+			return v, nil
+
+		case SignRequest:
+			v.UserVisibleData = encodedData
+			return v, nil
+		}
+
+		return nil, fmt.Errorf("unkown type: %T", rb)
+	}
 }
